bc484d962959386e310e4436327ee945ace2fe50
ISUSCT <mercurysolarcar@gmail.com>
2021-06-13 07:46:48 -0500

    Add files via upload

---
+++ Telemetry_win/Telemetry_win/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace Telemetry_win
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comPortCombo = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.openComBtn = new System.Windows.Forms.Button();
+            this.closeComBtn = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.baudRateCombo = new System.Windows.Forms.ComboBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.groupPowerTrack = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comPortCombo
+            // 
+            this.comPortCombo.FormattingEnabled = true;
+            this.comPortCombo.Location = new System.Drawing.Point(93, 13);
+            this.comPortCombo.Name = "comPortCombo";
+            this.comPortCombo.Size = new System.Drawing.Size(121, 21);
+            this.comPortCombo.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Port:";
+            // 
+            // openComBtn
+            // 
+            this.openComBtn.Location = new System.Drawing.Point(9, 78);
+            this.openComBtn.Name = "openComBtn";
+            this.openComBtn.Size = new System.Drawing.Size(75, 40);
+            this.openComBtn.TabIndex = 2;
+            this.openComBtn.Text = "Open Connection";
+            this.openComBtn.UseVisualStyleBackColor = true;
+            this.openComBtn.Click += new System.EventHandler(this.openComBtn_Click);
+            // 
+            // closeComBtn
+            // 
+            this.closeComBtn.Location = new System.Drawing.Point(139, 78);
+            this.closeComBtn.Name = "closeComBtn";
+            this.closeComBtn.Size = new System.Drawing.Size(75, 40);
+            this.closeComBtn.TabIndex = 3;
+            this.closeComBtn.Text = "Close Connection";
+            this.closeComBtn.UseVisualStyleBackColor = true;
+            this.closeComBtn.Click += new System.EventHandler(this.closeComBtn_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 43);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(61, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Baud Rate:";
+            // 
+            // baudRateCombo
+            // 
+            this.baudRateCombo.FormattingEnabled = true;
+            this.baudRateCombo.Location = new System.Drawing.Point(93, 40);
+            this.baudRateCombo.Name = "baudRateCombo";
+            this.baudRateCombo.Size = new System.Drawing.Size(121, 21);
+            this.baudRateCombo.TabIndex = 4;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.closeComBtn);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.openComBtn);
+            this.groupBox1.Controls.Add(this.comPortCombo);
+            this.groupBox1.Controls.Add(this.baudRateCombo);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(227, 135);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "COM";
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Location = new System.Drawing.Point(0, 512);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(1182, 22);
+            this.statusStrip1.TabIndex = 7;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // groupPowerTrack
+            // 
+            this.groupPowerTrack.Location = new System.Drawing.Point(245, 12);
+            this.groupPowerTrack.Name = "groupPowerTrack";
+            this.groupPowerTrack.Size = new System.Drawing.Size(291, 180);
+            this.groupPowerTrack.TabIndex = 8;
+            this.groupPowerTrack.TabStop = false;
+            this.groupPowerTrack.Text = "Power Trackers";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Location = new System.Drawing.Point(542, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(291, 180);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Pack Info";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(7, 30);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(7, 48);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 61);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 2;
+            this.label5.Text = "label5";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1182, 534);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupPowerTrack);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "ISU Solar Car Telematics";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comPortCombo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button openComBtn;
+        private System.Windows.Forms.Button closeComBtn;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox baudRateCombo;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.GroupBox groupPowerTrack;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Telemetry_win/Telemetry_win/Form1.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Telemetry_win
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            // init the com port combo box
+            string[] ports = SerialPort.GetPortNames();
+            comPortCombo.Items.AddRange(ports);
+            comPortCombo.SelectedIndex = 0;
+            closeComBtn.Enabled = false;
+
+            // init the baud rate combo box
+            string[] bauds = new String[14] { "110", "300", "600", "1200", "2400", "4800", "9600", "14400", "19200", "38400", "57600", "115200", "128000", "256000" };
+            baudRateCombo.Items.AddRange(bauds);
+            baudRateCombo.SelectedIndex = 10;
+        }
+
+        private void openComBtn_Click(object sender, EventArgs e)
+        {
+            openComBtn.Enabled = false;
+            closeComBtn.Enabled = true;
+            try
+            {
+                serialPort1.PortName = comPortCombo.Text;
+                serialPort1.BaudRate = int.Parse(baudRateCombo.Text);
+                serialPort1.Open();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Message", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void closeComBtn_Click(object sender, EventArgs e)
+        {
+            serialPort1.Close();
+            closeComBtn.Enabled = false;
+            openComBtn.Enabled = true;
+        }
+
+
+    }
+}
